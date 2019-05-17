7071d4ee4d6c0326a743e162d615be6742dec742
Gospodin <gchav@softserveinc.com>
2019-05-18 01:28:50 +0300

    changing folder structure

---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import Aux from "../Aux";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import classes from "./Layout.css";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
+
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  };
+
+  sideDrawerOpenHandler = () => {
+    this.setState({ showSideDrawer: true });
+  };
+  sideDrawerClosedHandler = () => {
+    this.setState(prevState => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
+  render() {
+    return (
+      <Aux>
+        <Toolbar drawerToggleClicked={this.sideDrawerOpenHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
