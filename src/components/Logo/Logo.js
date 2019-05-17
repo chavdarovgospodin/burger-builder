d8f9377fbc84ffe1de8d9cd6457f86aac3c621e7
Gospodin Chavdarov <gchav@softserveinc.com>
2019-05-17 14:15:39 +0300

    making the the side drawer and adjusting the height of the logo shown in both mobile and desktop

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import classes from './Logo.css'
 import burgerLogo from '../../assets/images/logo.jpg';
 
 const logo = (props) => (
-    <div className={classes.Logo}>
+    <div className={classes.Logo} style={{height: props.height}}>
         <img src={burgerLogo} alt="myBurger"/>
     </div>
 )
