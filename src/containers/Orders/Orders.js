3be40725a4c640dffc4f4e275e18cec394d0c8fb
Gospodin <gchav@softserveinc.com>
2019-05-26 13:58:56 +0300

    'fetching-the-orders-from-firebase'

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,12 +1,33 @@
-import React, { Component } from 'react';
-import Order from '../../components/Order/Order';
+import React, { Component } from "react";
+import Order from "../../components/Order/Order";
+import axios from "../../axios-orders";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 class Orders extends Component {
-    render () {
-        return (
-            <Order/>
-        )
-    }
+  state = {
+    orders: [],
+    loading: true
+  };
+  componentDidMount() {
+    const fetchedOrders = [];
+    axios
+      .get("/orders.json")
+      .then(res => {
+        for (let key in res.data) {
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key
+          });
+          this.setState({ loading: false, orders: fetchedOrders });
+        }
+      })
+      .catch(error => {
+        this.setState({ loading: false });
+      });
+  }
+  render() {
+    return <Order />;
+  }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
